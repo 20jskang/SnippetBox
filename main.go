ff56ff96a5b1cf322da39287486b35d0d0499963
20jskang <20jskang@gmail.com>
2026-09-20 23:00:57 +1000

    code: customising http responses

--- main.go
+++ main.go
@@ -10,6 +10,11 @@ import (
 // Define a home handler function which writes a byte slice containing
 // "Hello from SnippetBox" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
+	// Use the Header().Add() method to add a 'Server: G0' header to the 
+	// response header map. The first parameter is the header name, and
+	// the second parameter is the header value.
+	w.Header().Add("Server", "Go")
+
 	w.Write([]byte("Hello from SnippetBox"))
 }
 
@@ -26,8 +31,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 	// Use the fmt.Sprint() function to interpolate the id value with a
 	// message, then write it as the HTTP response.
-	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
-	w.Write([]byte(msg))
+	fmt.Fprintf(w, "Display a specific snipet with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -37,6 +41,10 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	// Use the w.WriteHeader() method to send a 201 status code.
+	w.WriteHeader(http.StatusCreated)
+
+	// Then use w.Write() method to write the response body as normal.
 	w.Write([]byte("Save a new snippet ..."))
 }
 
