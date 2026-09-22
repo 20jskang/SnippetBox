518bf1487737376b4be69a417027af85e18c0206
20jskang <20jskang@gmail.com>
2026-09-22 21:32:00 +1000

    code: logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// Define a new command-line flag with the name 'addr', a default value of ":4000"
+	// and some short help text explaining what the flag controls. The value of the
+	// flag will be stored in the addr variable at runtime.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Importantly, we use flag.Parse() function to parse the command-line flag.
+	// This reads in the command-line flag value and assigns it to the addr
+	// variable. You need to call this *before* you use the addr variable
+	// otherwise it will always contain the default value of ":4000". If any errors
+	// are encountered during parsing the application will be terminated.
+	flag.Parse()
+
+	// Use the slog.New() function to initialise a new structured logger, which
+	// writes to the standard out stream and uses the default settings.
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
 	// Use the http.NewServeMux() function to initialise a new servemux
 	mux := http.NewServeMux()
 
@@ -14,27 +32,40 @@ func main() {
 	// directory root.
 	fileServer := http.FileServer(http.Dir("./assets/static/"))
 
-	// Use the mux.Handle() function to register the file server as the handler for 
+	// Use the mux.Handle() function to register the file server as the handler for
 	// all URL paths that start with "/static/". For matching paths, we strip the
 	// "/static" prefix before the request reaches the file server.
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-	
+
 	// Register the other application routes as normal.
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-
 	// Print a log message to say that the server is starting
-	log.Print("starting server on :4000")
+	//
+	// The value returned from flag.String() function is a pointer to the flag
+	// value, not the value itself. So in this code, that means the addr variable
+	// is actually a pointer, and we need to dereference it before using it.
+	// Note that we're using the log.Printf() function to interpolate the address
+	// with the log message.
+	// 
+	// Use the Info() method to log the starting server message at Info severity
+	// (along with the listen address as an attribute).
+	logger.Info("Starting server", "addr", *addr)
 
 	// Use the http.ListenAndServe() function to start a new web server. We pass in
 	// two parameters: the TCP network address to listen on (in this case ":4000")
 	// and the servemux we just created. If http.ListenAndServe() returns an error
-	// we use the log.Fatal() function to log the error message and terminate the 
+	// we use the log.Fatal() function to log the error message and terminate the
 	// program. Note that any error returned by http.ListenAndServe() is always
 	// non-nil.
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	err := http.ListenAndServe(*addr, mux)
+
+	// And we also use the Error() method to log any error message returned by
+	// http.ListenAndServe() at Error severity (with no additional attributes),
+	// and then call os.Exit(1) to terminate the application with exit code 1.
+	logger.Error(err.Error())
+	os.Exit(1)
 }
